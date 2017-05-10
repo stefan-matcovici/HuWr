93f24ad657ec89ed16825d76ee1af6b52a74ae38
stefan-matcovici <stefan.matcovici@yahoo.com>
2017-05-10 19:49:35 +0300

    added https4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         Schema::defaultStringLength(191);
+        \URL::forceScheme('https');
     }
 
     /**
