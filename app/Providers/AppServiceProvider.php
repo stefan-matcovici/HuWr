7d7964719a2b5346e76f35e45f04f3d680f54288
stefan-matcovici <stefan.matcovici@yahoo.com>
2017-05-10 19:04:18 +0300

    added https calls

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceSchema('https');
+        }
     }
 
     /**
