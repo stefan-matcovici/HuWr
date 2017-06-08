4106f6aec33d752471a414fb80741758d68e036a
DESKTOP-LEVSIBV\Ariana <opariuc.ariana@gmail.com>
2017-06-08 14:48:44 +0300

    get nextdate for a year from DB

--- app/Http/Controllers/PredictionsController.php
+++ app/Http/Controllers/PredictionsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\HumanMigration as Migration;
 use Illuminate\Support\Facades\DB;
+use Redirect;
+use DateInterval;
 
 class PredictionsController extends Controller
 {
@@ -19,7 +21,31 @@ class PredictionsController extends Controller
         $migrations = DB::select("
         select * 
         from human_migrations
-        where (>=".$start." and arrival_country="."'".$arrivalCode."')");
+        where created_at>=".$start);
+
+        /*$migrations = DB::select("
+        select * 
+        from human_migrations
+        where (created_at>=".$start." and arrival_country="."'".$arrivalCode."')");*/
+        $date = date_create( $migrations[0]->created_at);
+        $date->add(new DateInterval('P1Y'));
+
+        dd( $date->format('Y-m-d H:i:s'));
         return $migrations;
+        //return Redirect::route("home");
+    }
+    public function getFuturePredictions($migrations)
+    {
+        $futurePredictions = [];
+        foreach($migrations as $migration)
+        {
+            if($migration->reason=="Personal" || $migration->reason=="Other")
+            {
+                $futurePredictions["created_at"]=$migration->created_at;
+                $futurePredictions["departure_country"]=$migration->departure_country;
+                $futurePredictions["departure_city"]=$migration->departure_city;
+            }
+        }
+
     }
 }
