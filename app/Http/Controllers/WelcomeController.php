4b99470fde781ec68fd712e9abc1705ff0a1a77e
stefan-matcovici <stefan.matcovici@yahoo.com>
2017-06-10 13:19:33 +0300

    added prediction for welcome map

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -19,7 +19,7 @@ class WelcomeController extends Controller
         $users = User::all();
 //        dd($users);
 //        dd($migrations);
-        return view('welcome.welcome',['migrations' => $migrations, 'users' => $users, 'prediction' => 1]);
+        return view('welcome.welcome',['migrations' => $migrations, 'users' => $users, 'prediction' => 2]);
     }
 
     public function feed()
