e06c4b96221d1a278108e24eff06f32dd8529273
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2017-06-12 21:23:10 +0300

    Styled a bit the guide.

--- resources/views/welcome/about.blade.php
+++ resources/views/welcome/about.blade.php
@@ -34,7 +34,7 @@
 
         </div>
         <div class="personal-information">
-            <img alt="avatar" class="personal-image" src="https://www.w3schools.com/w3css/img_avatar5.png">
+            <img alt="avatar" class="personal-image" src="https://scontent.fotp3-3.fna.fbcdn.net/v/t1.0-9/18486420_461102364237586_2877046768285819798_n.jpg?oh=2962f8410a5db4f48439e5f50f0a2006&oe=59DA0610">
             <h3>
                 Opariuc Ariana
             </h3>
